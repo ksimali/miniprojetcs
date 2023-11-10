7f2d9bd0e091be541628a9cc1dedcca6ccd2174d
Cassim <cassim.ali@outlook.fr>
2023-11-10 16:51:40 -0500

    Setting up Saisie() funtion: function which ask an identification code to the user.

--- miniprojetcs/Program.cs
+++ miniprojetcs/Program.cs
@@ -4,7 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Declaration des variables entier et tableaux.
+        int code;
+        // Appel de la fonction saisi()
+        code = Saisie();
+    }
+
+    //Fonction de saisie d'un nombre par l'utilisateur
+    public static int Saisie()
+    {
+        //Declaration d'une variable entier
+        int code;
+        // Saisie d'un nombre par l'utilisateur
+        Console.Write("Veuillez saisir votre code d'employé: ");
+        code = int.Parse(Console.ReadLine());
+
+        return code;
     }
 }
 
