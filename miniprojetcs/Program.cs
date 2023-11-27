8b56232d07a59565a8d04e3c991ebb6689f1f10e
Cassim <cassim.ali@outlook.fr>
2023-11-26 23:56:41 -0500

    Update display article in Facturation() method

--- miniprojetcs/Program.cs
+++ miniprojetcs/Program.cs
@@ -160,7 +160,7 @@ class Program
             foreach (string article in panier)
             {
                 int index = SearchString(codeArticle, article); // Verifie que le code article existe dans le tableau codeArticle et retourne l'index
-                Console.WriteLine("- "+ codeArticle[index] + ": " + nomArticle[index] + "           " + prixArticle[index] + "$");
+                Console.WriteLine($"- {codeArticle[index]} : { nomArticle[index]}{ prixArticle[index]}$");
             }
         }
 
